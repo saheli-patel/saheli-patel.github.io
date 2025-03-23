8e3dd4e202769ee28a968680efcb764514f127aa
vs-vivek <vsvivek99@gmail.com>
2025-03-23 22:04:05 +0000

    Deploying to gh-pages from @ saheli-patel/saheli-patel.github.io@2f8961f4f8bcc8872336fbe43f903b0a046896b9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -400,8 +400,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-fontan-project",
+          title: 'Fontan project',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
@@ -415,9 +415,9 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-book-list",
+          title: 'Book List',
+          description: "A page to track my reading",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
@@ -425,13 +425,13 @@ ninja.data = [{
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
+            },},{id: "projects-chessboard",
+          title: 'Chessboard',
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
+            },},{id: "projects-cookbook",
+          title: 'Cookbook',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
